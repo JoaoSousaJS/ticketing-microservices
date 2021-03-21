07294c4355cc87de2ef473ae12fd2a5842721abb
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-21 22:32:37 +1030

    feat: add current user router

---
+++ auth/src/infra/routes/auth-routes.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import { getCurrentUser } from '../../presentation/controllers/user/get-current-user';
+
+export const authRouter = express.Router();
+
+authRouter.get('/api/users/currentuser', getCurrentUser);
