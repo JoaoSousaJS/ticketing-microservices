27d2b88a1d36816d10a96dbf2651506c2c2bc487
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-22 23:40:15 +1030

    feat: add express-validator middleware to sign up route

--- auth/src/infra/routes/index.ts
+++ auth/src/infra/routes/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { body } from 'express-validator';
 import {
   getCurrentUser, signIn, signOut, signUp,
 } from '../../presentation/controllers/user';
@@ -6,6 +7,13 @@ import {
 export const authRouter = express.Router();
 
 authRouter.get('/api/users/currentuser', getCurrentUser);
+
 authRouter.post('/api/users/signin', signIn);
-authRouter.post('/api/users/signup', signUp);
+
+authRouter.post('/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
+  ], signUp);
+
 authRouter.post('/api/users/signout', signOut);
