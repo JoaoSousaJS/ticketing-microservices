1db017059089d67e0f1cb92193a5622e29f73913
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-22 23:12:19 +1030

    feat: add signup controller and route

--- auth/src/infra/routes/index.ts
+++ auth/src/infra/routes/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { getCurrentUser, signIn } from '../../presentation/controllers/user';
+import { getCurrentUser, signIn, signUp } from '../../presentation/controllers/user';
 
 export const authRouter = express.Router();
 
 authRouter.get('/api/users/currentuser', getCurrentUser);
 authRouter.get('/api/users/signin', signIn);
+authRouter.get('/api/users/signup', signUp);
