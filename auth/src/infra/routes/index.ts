d8674fec2a32310db86d66963942ccfec2b5e3f9
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-22 23:14:14 +1030

    feat: change to post method

--- auth/src/infra/routes/index.ts
+++ auth/src/infra/routes/index.ts
@@ -6,6 +6,6 @@ import {
 export const authRouter = express.Router();
 
 authRouter.get('/api/users/currentuser', getCurrentUser);
-authRouter.get('/api/users/signin', signIn);
-authRouter.get('/api/users/signup', signUp);
-authRouter.get('/api/users/signout', signOut);
+authRouter.post('/api/users/signin', signIn);
+authRouter.post('/api/users/signup', signUp);
+authRouter.post('/api/users/signout', signOut);
