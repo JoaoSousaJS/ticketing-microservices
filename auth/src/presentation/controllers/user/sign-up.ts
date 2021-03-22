9b82b01110545e8141cd32223534cd49ba8401fe
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-23 00:00:56 +1030

    feat: return 400 if there is error

--- auth/src/presentation/controllers/user/sign-up.ts
+++ auth/src/presentation/controllers/user/sign-up.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 
 export const signUp = (req: Request, res: Response) => {
-  res.send('Hi there');
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array());
+  }
+
+  console.log('Creating a user...');
+
+  return res.send({});
 };
