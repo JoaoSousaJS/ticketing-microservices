809e52b6ddeb8a4b83bb715d81b5f2b43811987e
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-22 23:13:11 +1030

    feat: add signout controller and router

--- auth/src/infra/routes/index.ts
+++ auth/src/infra/routes/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getCurrentUser, signIn, signUp } from '../../presentation/controllers/user';
+import {
+  getCurrentUser, signIn, signOut, signUp,
+} from '../../presentation/controllers/user';
 
 export const authRouter = express.Router();
 
 authRouter.get('/api/users/currentuser', getCurrentUser);
 authRouter.get('/api/users/signin', signIn);
 authRouter.get('/api/users/signup', signUp);
+authRouter.get('/api/users/signout', signOut);
