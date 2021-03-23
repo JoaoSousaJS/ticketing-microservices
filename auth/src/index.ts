a111004279d48415d351a614cf9d64ffa9c06577
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-24 00:07:28 +1030

    feat: call error handler middleware on index

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { authRouter } from './infra/routes';
+import { errorHandler } from './main/middlewares/error-handler';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(authRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('listening on port 3000!');
