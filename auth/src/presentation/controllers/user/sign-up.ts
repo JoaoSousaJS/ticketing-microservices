aa9eccfefbcf7d805d4219c8c3f9b592c0939540
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-25 23:08:23 +1030

    feat: add user model to sign up controller

--- auth/src/presentation/controllers/user/sign-up.ts
+++ auth/src/presentation/controllers/user/sign-up.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
-import { DatabaseConnectionError, RequestValidationError } from '../../errors';
+import { User } from '../../../infra/database/models/user';
+import { RequestValidationError } from '../../errors';
 
-export const signUp = (req: Request, res: Response) => {
-  const errors = validationResult(req);
+export const signUp = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-  }
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
 
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
+    const { email, password } = req.body;
 
-  // return res.send({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        console.log('Email in use');
+        return res.send({});
+    }
+    const user = await User.create({
+        email,
+        password,
+    });
+
+    res.status(201).send(user);
 };
