1afdc8fb5b4ff0c69b1fe2051fb7dfbd92fc419f
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-25 23:08:10 +1030

    refactor: lol whitespace

--- auth/src/infra/database/models/user/index.ts
+++ auth/src/infra/database/models/user/index.ts
@@ -6,14 +6,14 @@ interface UserAttrs extends Document{
 }
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
 });
 
 export const User = mongoose.model<UserAttrs>('User', userSchema);
