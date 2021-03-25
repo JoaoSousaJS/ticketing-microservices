266c6723d374eacb25450f8b333ef0b4453f07bf
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-25 19:58:29 +1030

    feat: create user model

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -1,36 +1,38 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', 'src/'],
-      },
+    env: {
+        es2021: true,
+        node: true,
     },
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-    'no-console': 'off',
-    'import/prefer-default-export': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/consistent-type-definitions': 'off',
-    '@typescript-eslint/comma-spacing': 'off',
-    '@typescript-eslint/return-await': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    'import/extensions': 'off',
-  },
+    extends: [
+        'airbnb-base',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                moduleDirectory: ['node_modules', 'src/'],
+            },
+        },
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+        'no-console': 'off',
+        'import/prefer-default-export': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/comma-spacing': 'off',
+        '@typescript-eslint/return-await': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        'import/extensions': 'off',
+        indent: 'off',
+        '@typescript-eslint/indent': ['error'],
+    },
 };
