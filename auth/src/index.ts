99c0981a9728adce29d680e12c6e42ebada84017
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-27 15:58:45 +1030

    feat: add cookie session to server

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cookieSession from 'cookie-session';
 import mongoose from 'mongoose';
 import { authRouter } from './infra/routes';
 import { errorHandler } from './main/middlewares/error-handler';
@@ -7,29 +8,36 @@ import { NotFoundError } from './presentation/errors';
 
 const app = express();
 
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    }),
+);
 
 app.use(authRouter);
 
 app.all('*', () => {
-  throw new NotFoundError();
+    throw new NotFoundError();
 });
 app.use(errorHandler);
 
 const start = async () => {
-  try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        console.log('connected to mongodb');
+    } catch (err) {
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log('listening on port 3000!');
     });
-    console.log('connected to mongodb');
-  } catch (err) {
-    console.error(err);
-  }
-  app.listen(3000, () => {
-    console.log('listening on port 3000!');
-  });
 };
 
 start();
