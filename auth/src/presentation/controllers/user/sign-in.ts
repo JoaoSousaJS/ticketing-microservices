ed925c2236d3150e39e441f25c91c48f9f803425
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-28 12:23:43 +1030

    feat: check if user exist and password is correct, return jwt token on 200

--- auth/src/presentation/controllers/user/sign-in.ts
+++ auth/src/presentation/controllers/user/sign-in.ts
@@ -1,5 +1,30 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from '../../../infra/database/models/user';
+import { Password } from '../../../infra/database/service/password';
+import { BadRequestError } from '../../errors';
 
-export const signIn = (req: Request, res: Response) => {
-  res.send('Hi there');
+export const signIn = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+        throw new BadRequestError('Invalid credentials');
+    }
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+
+    if (!passwordsMatch) {
+        throw new BadRequestError('Invalid credentials');
+    }
+
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email,
+    }, process.env.JWT_KEY);
+
+    req.session = {
+        jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
 };
