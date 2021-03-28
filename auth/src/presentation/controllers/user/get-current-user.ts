be5a6f3b1521ba9862b134f3de283bfb98f459d7
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-28 14:32:49 +1030

    refactor: remove logic to middleware

--- auth/src/presentation/controllers/user/get-current-user.ts
+++ auth/src/presentation/controllers/user/get-current-user.ts
@@ -1,15 +1,5 @@
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
 export const getCurrentUser = (req: Request, res: Response) => {
-    if (!req.session?.jwt) {
-        return res.send({ currentUser: null });
-    }
-
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY);
-        res.send({ currentUser: payload });
-    } catch (error) {
-        res.send({ currentUser: null });
-    }
+    res.send({ currentUser: req.currentUser || null });
 };
