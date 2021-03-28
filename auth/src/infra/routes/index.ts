19d492c11095a27b3aa7fcc5a18f2d782efcc521
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-28 12:08:21 +1030

    feat: add validate-request middleware to sign up and sign in route

--- auth/src/infra/routes/index.ts
+++ auth/src/infra/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { body } from 'express-validator';
+import { validateRequest } from '../../main/middlewares';
 import {
     getCurrentUser, signIn, signOut, signUp,
 } from '../../presentation/controllers/user';
@@ -11,12 +12,12 @@ authRouter.get('/api/users/currentuser', getCurrentUser);
 authRouter.post('/api/users/signin', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().notEmpty().withMessage('You must supply a password'),
-], signIn);
+], validateRequest, signIn);
 
 authRouter.post('/api/users/signup',
     [
         body('email').isEmail().withMessage('Email must be valid'),
         body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
-    ], signUp);
+    ], validateRequest, signUp);
 
 authRouter.post('/api/users/signout', signOut);
