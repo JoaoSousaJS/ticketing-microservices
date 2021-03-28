2b6562504bcfaca1d2a47ac8d83ed70c6e6365e6
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-28 11:56:26 +1030

    feat: email and password required for signin

--- auth/src/infra/routes/index.ts
+++ auth/src/infra/routes/index.ts
@@ -1,19 +1,22 @@
 import express from 'express';
 import { body } from 'express-validator';
 import {
-  getCurrentUser, signIn, signOut, signUp,
+    getCurrentUser, signIn, signOut, signUp,
 } from '../../presentation/controllers/user';
 
 export const authRouter = express.Router();
 
 authRouter.get('/api/users/currentuser', getCurrentUser);
 
-authRouter.post('/api/users/signin', signIn);
+authRouter.post('/api/users/signin', [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('You must supply a password'),
+], signIn);
 
 authRouter.post('/api/users/signup',
-  [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
-  ], signUp);
+    [
+        body('email').isEmail().withMessage('Email must be valid'),
+        body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
+    ], signUp);
 
 authRouter.post('/api/users/signout', signOut);
