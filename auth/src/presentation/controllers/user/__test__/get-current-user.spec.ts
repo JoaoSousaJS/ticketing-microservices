87ff9266e2f112f6cf3a9e0b8c6e804a26bfd32d
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-30 19:53:13 +1030

    test: add global sign in to current user test

--- auth/src/presentation/controllers/user/__test__/get-current-user.spec.ts
+++ auth/src/presentation/controllers/user/__test__/get-current-user.spec.ts
@@ -11,14 +11,10 @@ describe('Current user Controller', () => {
     afterAll(async () => close());
 
     it('should return the details of the current user', async () => {
-        const authResponse = await agent.post('/api/users/signup').send({
-            email: 'test@test23.com',
-            password: '1234',
-        }).expect(201);
-        const cookie = authResponse.get('Set-Cookie')[0];
+        const cookie = await global.signin();
 
         const response = await agent.get('/api/users/currentuser').set('Cookie', cookie).send().expect(200);
 
-        expect(response.body.currentUser.email).toEqual('test@test23.com');
+        expect(response.body.currentUser.email).toEqual('test@test.com');
     });
 });
