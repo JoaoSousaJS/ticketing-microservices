f2b058830ef4fcd399f2bac5687ae97361e6d181
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-30 19:51:12 +1030

    feat: add global signin for test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable guard-for-in */
@@ -5,6 +6,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): Promise<string>
+        }
+    }
+}
 
 const mongoServer = new MongoMemoryServer();
 
@@ -40,4 +51,17 @@ export const clear = async () => {
     for (const key in collections) {
         await collections[key].deleteMany({});
     }
+};
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app).post('/api/users/signup').send({
+        email, password,
+    }).expect(201);
+
+    const cookie = response.get('Set-Cookie')[0];
+
+    return cookie;
 };
