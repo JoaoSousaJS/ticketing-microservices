abaa62ca32036b45d613ef17b731c868bc10db67
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-30 12:38:53 +1030

    test: fail if password is incorrect

--- auth/src/presentation/controllers/user/__test__/sign-in.spec.ts
+++ auth/src/presentation/controllers/user/__test__/sign-in.spec.ts
@@ -10,10 +10,22 @@ describe('Sign In Controller', () => {
     beforeEach(async () => clear());
     afterAll(async () => close());
 
-    it('should fails where a email that does not exist is supplied', async () => {
+    it('should fail where a email that does not exist is supplied', async () => {
         await agent.post('/api/users/signin').send({
             email: 'test@test23.com',
             password: '1234',
+        }).expect(400);
+    });
+
+    it('should fail when an incorrect password is supplied', async () => {
+        await agent.post('/api/users/signup').send({
+            email: 'test@test23.com',
+            password: '1234',
+        }).expect(201);
+
+        await agent.post('/api/users/signin').send({
+            email: 'test@test23.com',
+            password: '1235',
         }).expect(400);
     });
 });
