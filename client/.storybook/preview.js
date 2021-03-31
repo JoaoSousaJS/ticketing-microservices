3672d8ca6aef32817383485f2815ec9ef4a646fa
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-31 16:10:18 +1030

    chore: change .storybook to load the theme

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,10 +1,15 @@
+import { ThemeProvider } from 'styled-components'
 import {GlobalStyles} from '../src/styles/global'
+import theme from 'styles/theme'
 
 export const decorators = [
   (Story) => (
     <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
+    </ThemeProvider>
+
     </>
   )
 ]
