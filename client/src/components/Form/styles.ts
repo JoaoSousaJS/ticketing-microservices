966bafaf135b884e8dded9ffecb4b4144710adfc
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-01 23:06:11 +1030

    feat: add max-width to form

--- client/src/components/Form/styles.ts
+++ client/src/components/Form/styles.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 export const Wrapper = styled.form`
   display: flex;
   flex-direction: column;
-  width: 50rem;
+  width: 40rem;
+  max-width: 50rem;
   margin-left: auto;
   margin-right: auto;
 `
