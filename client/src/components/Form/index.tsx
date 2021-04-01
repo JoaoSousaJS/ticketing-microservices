8779ec5c7b7b91307c399bf114db60485a8da8f7
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-02 00:13:37 +1030

    feat: add error style

--- client/src/components/Form/index.tsx
+++ client/src/components/Form/index.tsx
@@ -13,21 +13,35 @@ type FormProps = {
     | undefined
   errors: {
     message: string
+    field: string
   }[]
 }
 
-const Form = ({ buttonText, onSubmit, reference, errors }: FormProps) => (
-  <S.Wrapper onSubmit={onSubmit}>
-    <Input label="Email" reference={reference} name="email" type="text" />
-    <Input
-      label="Password"
-      reference={reference}
-      name="password"
-      type="password"
-    />
-    {errors && errors.map((err) => err.message)}
-    <Button size="medium">{buttonText}</Button>
-  </S.Wrapper>
-)
+const Form = ({ buttonText, onSubmit, reference, errors }: FormProps) => {
+  return (
+    <S.Wrapper onSubmit={onSubmit}>
+      <Input label="Email" reference={reference} name="email" type="text" />
+      <Input
+        label="Password"
+        reference={reference}
+        name="password"
+        type="password"
+      />
+
+      {errors.length >= 1 && (
+        <S.MessageContainer>
+          <S.MessageHeading>Ooooops...</S.MessageHeading>
+          <S.MessageList>
+            {errors.map((err) => (
+              <S.MessageItem key={err.field}>{err.message}</S.MessageItem>
+            ))}
+          </S.MessageList>
+        </S.MessageContainer>
+      )}
+
+      <Button size="medium">{buttonText}</Button>
+    </S.Wrapper>
+  )
+}
 
 export default Form
--- client/src/components/Form/styles.ts
+++ client/src/components/Form/styles.ts
@@ -8,3 +8,18 @@ export const Wrapper = styled.form`
   margin-left: auto;
   margin-right: auto;
 `
+
+export const MessageContainer = styled.div`
+  background-color: #f5646c;
+  margin-bottom: 1rem;
+`
+
+export const MessageHeading = styled.h4`
+  font-size: 2.5rem;
+`
+
+export const MessageList = styled.ul``
+
+export const MessageItem = styled.li`
+  font-size: 1.5rem;
+`
