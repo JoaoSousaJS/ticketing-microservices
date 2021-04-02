50b0847cfb2378bf1f60fc354f03fdac3e77fa01
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-02 23:22:15 +1030

    feat: add use request call

--- client/src/pages/auth/signup.tsx
+++ client/src/pages/auth/signup.tsx
@@ -1,7 +1,6 @@
-import axios from 'axios'
 import Form from 'components/Form'
-import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import useRequest from '../../hooks/use-request'
 
 type FormData = {
   email: string
@@ -10,18 +9,13 @@ type FormData = {
 
 export default function Signup() {
   const { handleSubmit, register } = useForm<FormData>()
-  const [errors, setErrors] = useState([])
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post'
+  })
 
   const handleSignUp = handleSubmit(async ({ email, password }) => {
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password
-      })
-      console.log(response.data)
-    } catch (error) {
-      setErrors(error.response.data.errors)
-    }
+    await doRequest({ email, password })
   })
 
   return (
