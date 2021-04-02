9be129c3b866bdd16fda419eeb6b37fcd74ccbdb
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-02 23:04:44 +1030

    feat: add use request custom hook

---
+++ client/src/hooks/use-request.tsx
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { useState } from 'react'
+import * as S from '../components/Form/styles'
+
+type ErrorsProps = {
+  message: string
+  field: string
+}
+
+type AxiosProps = {
+  method: 'get' | 'post' | 'patch'
+  url: string
+  body: string
+}
+
+export default ({ url, method, body }: AxiosProps) => {
+  const [errors, setErrors] = useState<JSX.Element>()
+
+  const doRequest = async () => {
+    try {
+      const response = await axios[method](url, body)
+      return response.data
+    } catch (error) {
+      setErrors(
+        <S.MessageContainer>
+          <S.MessageHeading>Ooooops...</S.MessageHeading>
+          <S.MessageList>
+            {error.response.data.errors.map((err: ErrorsProps) => (
+              <S.MessageItem key={err.field}>{err.message}</S.MessageItem>
+            ))}
+          </S.MessageList>
+        </S.MessageContainer>
+      )
+    }
+  }
+
+  return { doRequest, errors }
+}
