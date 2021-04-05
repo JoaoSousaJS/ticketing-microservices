4b74fed43b1aaa66a1e989dcc29f984e8f53cafe
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-05 13:01:27 +0930

    feat: check if the request is on client or server

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,42 @@
+import axios from 'axios'
 import { Main } from 'components/Main'
+import { GetServerSideProps } from 'next'
 
-export default function Home() {
+type HomeProps = {
+  currentUser: {
+    id: string
+    email: string
+    iat: string
+  }
+}
+
+export default function Home(props: HomeProps) {
+  console.log(props)
   return <Main />
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev'
+        }
+      }
+    )
+
+    return {
+      props: {
+        data
+      }
+    }
+  } else {
+    const { data } = await axios.get('/api/users/currentuser')
+    return {
+      props: {
+        data
+      }
+    }
+  }
 }
