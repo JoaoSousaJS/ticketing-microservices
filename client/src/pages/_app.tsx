93b9992149cf17437c1fe290fbb051925cd7a306
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-06 19:43:41 +0930

    feat: add base to _app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,12 @@
-import { AppProps } from 'next/app'
+import { buildClient } from 'api/build-client'
+import Base from 'components/Base'
+import { AppContext, AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from 'styles/global'
 import theme from 'styles/theme'
 
-function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps, currentUser }: AppProps) => {
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -18,11 +20,29 @@ function App({ Component, pageProps }: AppProps) {
             content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
           />
         </Head>
+        <Base currentUser={currentUser} />
         <GlobalStyles />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
   )
+}
+
+App.getInitialProps = async (appContext: AppContext) => {
+  const client = buildClient(appContext.ctx)
+  const { data } = await client.get('/api/users/currentuser')
+
+  let pageProps = {}
+
+  console.log(appContext.Component.getInitialProps)
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+  }
+  return {
+    pageProps,
+    currentUser: data.currentUser
+  }
 }
 
 export default App
