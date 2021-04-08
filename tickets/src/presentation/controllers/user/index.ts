6bf95863809fddf595f539e2cbbbdc79dc080ab2
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-08 19:23:04 +0930

    feat: remove unused folders

--- tickets/src/presentation/controllers/user/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './get-current-user';
-export * from './sign-in';
-export * from './sign-up';
-export * from './sign-out';
--- tickets/src/presentation/controllers/user/sign-out.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-
-export const signOut = (req: Request, res: Response) => {
-    // @ts-ignore
-    req.session = null;
-
-    res.send({});
-};
