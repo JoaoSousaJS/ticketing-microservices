0c5342b6d6180b520528add2507115490b6535ef
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-09 00:08:27 +0930

    test: should not return 404

---
+++ tickets/src/routes/__test__/new.spec.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+import request from 'supertest';
+import { app } from '../../app';
+import { clear, connect, close } from '../../test/setup';
+
+const agent = request.agent(app);
+
+describe('New Ticket', () => {
+    beforeAll(async () => connect());
+    beforeEach(async () => clear());
+    afterAll(async () => close());
+
+    it('should handler listening to /api/tickets for post requests', async () => {
+        const response = await agent.post('/api/tickets').send({
+        });
+
+        expect(response.status).not.toEqual(404);
+    });
+
+    it('should only be accessed if user is signed in', async () => {
+    });
+
+    it('should returns an error if an invalid title is provided', async () => {
+    });
+
+    it('should returns an error if an invalid price is provided', async () => {
+    });
+
+    it('should creates a ticket with valid inputs', async () => {
+    });
+});
