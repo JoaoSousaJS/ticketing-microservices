a2d57546cddd7c28bb83719f867f2ade1a347fe3
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-09 12:18:23 +0930

    feat: return 401 if user is not authorized

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@htickets/common';
+import { currentUser, errorHandler, NotFoundError } from '@htickets/common';
 import { router } from './routes/new';
 
 export const app = express();
@@ -14,6 +14,8 @@ app.use(
         secure: process.env.NODE_ENV !== 'test',
     }),
 );
+
+app.use(currentUser);
 
 app.use(router);
 
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
+import { requireAuth } from '@htickets/common';
 import express from 'express';
 import { newTicket } from '../controllers/tickets/new-ticket';
 
 export const router = express.Router();
 
-router.post('/api/tickets', newTicket);
+router.post('/api/tickets', requireAuth, newTicket);
