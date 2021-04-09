878ba814e6ce35206cf137732cba809b4c1dc0f8
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-09 19:55:54 +0930

    feat: check if title and price are valid

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,7 +1,13 @@
-import { requireAuth } from '@htickets/common';
+import { requireAuth, validateRequest } from '@htickets/common';
 import express from 'express';
+import { body } from 'express-validator';
 import { newTicket } from '../controllers/tickets/new-ticket';
 
 export const router = express.Router();
 
-router.post('/api/tickets', requireAuth, newTicket);
+router.post('/api/tickets', requireAuth, [
+    body('title').notEmpty().withMessage('Title is required'),
+    body('price').notEmpty().isFloat({
+        gt: 0,
+    }).withMessage('Price is required'),
+], validateRequest, newTicket);
