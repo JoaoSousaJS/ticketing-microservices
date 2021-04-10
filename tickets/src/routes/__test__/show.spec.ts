69194d0020a41922d435f68af7e86503839e1b49
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-10 20:27:34 +0930

    test: should rethr the ticket if it is found

--- tickets/src/routes/__test__/show.spec.ts
+++ tickets/src/routes/__test__/show.spec.ts
@@ -10,10 +10,22 @@ describe('Show Ticket', () => {
     beforeEach(async () => clear());
     afterAll(async () => close());
 
+    const title = 'title';
+    const price = 20;
+
     it('should returns 404 if the ticket is not found', async () => {
         await agent.get('/api/tickets/fakeId').send().expect(404);
     });
 
     it('should returns the ticket if the ticket is found', async () => {
+        const response = await agent.post('/api/tickets').set('Cookie', global.signin()).send({
+            title,
+            price,
+        }).expect(201);
+
+        const ticketresponse = await agent.get(`/api/tickets/${response.body.id}`).send().expect(200);
+
+        expect(ticketresponse.body.title).toEqual(title);
+        expect(ticketresponse.body.price).toEqual(price);
     });
 });
