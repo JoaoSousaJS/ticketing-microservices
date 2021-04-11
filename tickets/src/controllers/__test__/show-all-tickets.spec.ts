8b19e50b840ce96d6c43b627a9c8a7fdea6de137
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-11 13:47:02 +0930

    test: show return a list of tickets

---
+++ tickets/src/controllers/__test__/show-all-tickets.spec.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+import request from 'supertest';
+import { app } from '../../app';
+import { clear, connect, close } from '../../test/setup';
+
+const agent = request.agent(app);
+
+const title = 'title';
+const price = 20;
+
+const createTicket = () => agent.post('/api/tickets').set('Cookie', global.signin()).send({
+    title,
+    price,
+}).expect(201);
+
+describe('Show Ticket', () => {
+    beforeAll(async () => connect());
+    beforeEach(async () => clear());
+    afterAll(async () => close());
+
+    it('should returns a list of tickets', async () => {
+        await createTicket();
+        await createTicket();
+        await createTicket();
+
+        const ticketresponse = await agent.get('/api/tickets/').send().expect(200);
+
+        expect(ticketresponse.body.length).toEqual(3);
+    });
+});
