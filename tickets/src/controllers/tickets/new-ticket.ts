9e104966a507d6657c4b4bed5d7e38aa7c5bdc22
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-21 22:48:35 +0930

    feat: send ticket created to nats

--- tickets/src/controllers/tickets/new-ticket.ts
+++ tickets/src/controllers/tickets/new-ticket.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { Ticket } from '../../database/model/ticket';
+import { TicketCreatedPublisher } from '../../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../../nats-wrapper';
 
 export const newTicket = async (req: Request, res: Response) => {
     const { title, price } = req.body;
@@ -11,6 +13,12 @@ export const newTicket = async (req: Request, res: Response) => {
     });
 
     await ticket.save();
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
 };
