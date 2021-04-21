50d563554bd6d9ef3466720d1261b6e7c5d8a93b
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-21 23:25:24 +0930

    feat: add ticketUpdatedPublisher event to update ticket controller

--- tickets/src/controllers/tickets/update-ticket.ts
+++ tickets/src/controllers/tickets/update-ticket.ts
@@ -1,6 +1,8 @@
 import { NotAuthorizedError, NotFoundError } from '@htickets/common';
 import { Request, Response } from 'express';
 import { Ticket } from '../../database/model/ticket';
+import { TicketUpdatedPublisher } from '../../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../../nats-wrapper';
 
 export const updateTicket = async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
@@ -19,6 +21,11 @@ export const updateTicket = async (req: Request, res: Response) => {
     });
 
     await ticket.save();
-
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId,
+    });
     res.send(ticket);
 };
