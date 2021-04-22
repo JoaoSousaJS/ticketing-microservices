3fc9ef05e727069d2b38d51287605dc3a12677f9
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-22 17:01:00 +0930

    feat: add order routes

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
-import { getAllOrders } from '../controllers/orders/get-all-orders';
+import {
+    deleteOrder, getAllOrders, getOrder, newOrder,
+} from '../controllers/orders';
 
 export const router = express.Router();
 
 router.get('/api/orders', getAllOrders);
+router.post('/api/orders', newOrder);
+router.get('/api/orders/:id', getOrder);
+router.delete('/api/orders/:id', deleteOrder);
