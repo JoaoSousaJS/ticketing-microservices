f50b329f2b9705244db3814c0626da262e55a2d0
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-23 19:57:54 +0930

    refactor: typo

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -9,7 +9,7 @@ import {
 export const router = express.Router();
 
 router.get('/api/orders', requireAuth, validateRequest, [
-    body('ticketId').notEmpty().custom((input:string) => mongoose.Types.ObjectId.isValid(input)).withMessage('TichetId must be provided'),
+    body('ticketId').notEmpty().custom((input:string) => mongoose.Types.ObjectId.isValid(input)).withMessage('TicketId must be provided'),
 ], getAllOrders);
 router.post('/api/orders', newOrder);
 router.get('/api/orders/:orderId', getOrder);
