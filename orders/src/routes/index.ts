4a1fb802f935a960519755daeac83306afa34541
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-23 10:18:08 +0930

    feat: add validation to get all orders route

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,11 +1,16 @@
+import mongoose from 'mongoose';
+import { requireAuth, validateRequest } from '@htickets/common';
 import express from 'express';
+import { body } from 'express-validator';
 import {
     deleteOrder, getAllOrders, getOrder, newOrder,
 } from '../controllers/orders';
 
 export const router = express.Router();
 
-router.get('/api/orders', getAllOrders);
+router.get('/api/orders', requireAuth, validateRequest, [
+    body('ticketId').notEmpty().custom((input:string) => mongoose.Types.ObjectId.isValid(input)).withMessage('TichetId must be provided'),
+], getAllOrders);
 router.post('/api/orders', newOrder);
 router.get('/api/orders/:orderId', getOrder);
 router.delete('/api/orders/:orderId', deleteOrder);
