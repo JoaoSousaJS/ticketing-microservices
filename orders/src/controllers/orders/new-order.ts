528b7c0b102748a3b9093551b01b92fe0adc9688
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-23 21:55:45 +0930

    feat: check if ticket and order exist

--- orders/src/controllers/orders/new-order.ts
+++ orders/src/controllers/orders/new-order.ts
@@ -1,5 +1,26 @@
+import { BadRequestError, NotFoundError, OrderStatus } from '@htickets/common';
 import { Request, Response } from 'express';
+import { Order } from '../../models/orders/orders';
+import { Ticket } from '../../models/tickets/tickets';
 
-export const newOrder = (req: Request, res: Response) => {
+export const newOrder = async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+        throw new NotFoundError();
+    }
+
+    const existingOrder = await Order.findOne({
+        ticket,
+        status: {
+            $in: [OrderStatus.Created, OrderStatus.AwaitingPament, OrderStatus.Complete],
+        },
+    });
+
+    if (existingOrder) {
+        throw new BadRequestError('Thicket is already reserved');
+    }
     res.send({});
 };
