b187f6da2ad38c32e67a87afe3d1717ec2e3441e
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-24 23:11:56 +0930

    feat: change order status when deleting an order

--- orders/src/controllers/orders/delete-order.ts
+++ orders/src/controllers/orders/delete-order.ts
@@ -1,5 +1,20 @@
+import { NotAuthorizedError, NotFoundError, OrderStatus } from '@htickets/common';
 import { Request, Response } from 'express';
+import { Order } from '../../models/orders/orders';
 
-export const deleteOrder = (req: Request, res: Response) => {
-    res.send({});
+export const deleteOrder = async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId).populate('ticket');
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser.id) {
+        throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+
+    await order.save();
+    res.status(204).send(order);
 };
