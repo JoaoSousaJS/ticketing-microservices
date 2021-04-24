452a02fa162ee63fc9745b32d7c8adfc95dc8a3e
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-24 22:40:23 +0930

    test: should fetch the order

---
+++ orders/src/controllers/__test__/get-order.spec.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/tickets/tickets';
+import { clear, connect, close } from '../../test/setup';
+
+const agent = request.agent(app);
+
+jest.mock('../../nats-wrapper');
+
+describe('New Order', () => {
+    beforeAll(async () => connect());
+    beforeEach(async () => clear());
+    afterAll(async () => close());
+
+    it('should fetch the order', async () => {
+        const ticket = await Ticket.create({
+            title: 'concert',
+            price: 20,
+        });
+
+        const user = global.signin();
+
+        const { body: order } = await agent.post('/api/orders').set('Cookie', user).send({
+            ticketId: ticket.id,
+        }).expect(201);
+
+        const { body: fetchedOrder } = await agent.get(`/api/orders/${order.id}`).set('Cookie', user).send().expect(200);
+
+        expect(fetchedOrder.id).toEqual(order.id);
+    });
+});
