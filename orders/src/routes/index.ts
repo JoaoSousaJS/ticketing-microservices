878cd9054dabeabf31b542fa5bc3c889e0dfc8d3
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-24 19:28:42 +0930

    feat: add authentication to get all orders

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -11,6 +11,6 @@ export const router = express.Router();
 router.post('/api/orders', requireAuth, validateRequest, [
     body('ticketId').notEmpty().custom((input:string) => mongoose.Types.ObjectId.isValid(input)).withMessage('TicketId must be provided'),
 ], newOrder);
-router.get('/api/orders', getAllOrders);
+router.get('/api/orders', requireAuth, getAllOrders);
 router.get('/api/orders/:orderId', getOrder);
 router.delete('/api/orders/:orderId', deleteOrder);
