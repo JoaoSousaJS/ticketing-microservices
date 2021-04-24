00ee554b765b8564200deed58e0c7b142333a563
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-24 19:30:31 +0930

    feat: get all orders of the current signed user

--- orders/src/controllers/orders/get-all-orders.ts
+++ orders/src/controllers/orders/get-all-orders.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
+import { Order } from '../../models/orders/orders';
 
-export const getAllOrders = (req: Request, res: Response) => {
-    res.send({});
+export const getAllOrders = async (req: Request, res: Response) => {
+    const orders = await Order.find({
+        userId: req.currentUser.id,
+    }).populate('ticket');
+
+    res.send(orders);
 };
