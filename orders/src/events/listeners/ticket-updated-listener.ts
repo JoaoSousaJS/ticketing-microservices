3a0cf58cafb9b9d45098fe8218bb3d0142573d11
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-25 19:41:06 +0930

    feat: add ticket-updated-listener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,30 @@
+import { Listener, Subjects, TicketUpdatedEvent } from '@htickets/common';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/tickets/tickets';
+import { queueGroupName } from './types/queue-group-name';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated
+
+    queueGroupName = queueGroupName
+
+    // eslint-disable-next-line class-methods-use-this
+    async onMessage(data: TicketUpdatedEvent['data'], msg:Message) {
+        const ticket = await Ticket.findById(data.id);
+
+        if (!ticket) {
+            throw new Error('Ticket not found');
+        }
+
+        const { title, price } = data;
+
+        ticket.set({
+            title,
+            price,
+        });
+
+        await ticket.save();
+
+        msg.ack();
+    }
+}
