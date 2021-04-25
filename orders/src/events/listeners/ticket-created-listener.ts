75394a56766123650ff362fe6f1ea28631af9791
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-25 15:54:44 +0930

    feat: add ticket created listener

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,21 @@
+import { Listener, Subjects, TicketCreatedEvent } from '@htickets/common';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/tickets/tickets';
+import { queueGroupName } from './types/queue-group-name';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    subject: Subjects.TicketCreated = Subjects.TicketCreated
+
+    queueGroupName = queueGroupName
+
+    // eslint-disable-next-line class-methods-use-this
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+        const { title, price } = data;
+        await Ticket.create({
+            title,
+            price,
+        });
+
+        msg.ack();
+    }
+}
