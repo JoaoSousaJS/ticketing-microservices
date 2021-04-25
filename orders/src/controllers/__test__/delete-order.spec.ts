912bb470a5a2a7cdd737bf5c14d4f9f2f155cf3f
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-25 13:57:42 +0930

    test: emit an event when an order is cancelled

--- orders/src/controllers/__test__/delete-order.spec.ts
+++ orders/src/controllers/__test__/delete-order.spec.ts
@@ -5,6 +5,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/orders/orders';
 import { Ticket } from '../../models/tickets/tickets';
+import { natsWrapper } from '../../nats-wrapper';
 import { clear, connect, close } from '../../test/setup';
 
 const agent = request.agent(app);
@@ -31,5 +32,22 @@ describe('Delete Order', () => {
         await agent.delete(`/api/orders/${order.id}`).set('Cookie', user).send().expect(204);
         const fetchedOrder = await Order.findById(order.id);
         expect(fetchedOrder.status).toEqual(OrderStatus.Cancelled);
+    });
+
+    it('should mark an order as cancelled', async () => {
+        const ticket = await Ticket.create({
+            title: 'concert',
+            price: 20,
+        });
+
+        const user = global.signin();
+
+        const { body: order } = await agent.post('/api/orders').set('Cookie', user).send({
+            ticketId: ticket.id,
+        }).expect(201);
+
+        await agent.delete(`/api/orders/${order.id}`).set('Cookie', user).send().expect(204);
+
+        expect(natsWrapper.client.publish).toHaveBeenCalled();
     });
 });
