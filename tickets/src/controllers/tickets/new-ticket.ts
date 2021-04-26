e4346c2c1e6fedc3682506b54d66adc0fac03f25
Joao Sousa <joao.rsousa23@gmail.com>
2021-04-26 19:54:39 +0930

    feat: add version to to new ticket

--- tickets/src/controllers/tickets/new-ticket.ts
+++ tickets/src/controllers/tickets/new-ticket.ts
@@ -18,6 +18,7 @@ export const newTicket = async (req: Request, res: Response) => {
         title: ticket.title,
         price: ticket.price,
         userId: ticket.userId,
+        version: ticket.version,
     });
 
     res.status(201).send(ticket);
