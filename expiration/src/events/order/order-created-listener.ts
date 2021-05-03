37044869cbecad473f8ed2f11044d881610f1b45
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-03 16:28:13 +0930

    feat: add OrderCreatedListener

---
+++ expiration/src/events/order/order-created-listener.ts
@@ -0,0 +1,19 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@htickets/common';
+import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
+import { queueGroupName } from '../queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated
+
+    queueGroupName = queueGroupName
+
+    // eslint-disable-next-line class-methods-use-this
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        await expirationQueue.add({
+            orderId: data.id,
+        });
+
+        msg.ack();
+    }
+}
