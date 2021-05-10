61ea149d5734c2ef1db59be9cb8f4b8a68114f98
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-11 00:05:47 +0930

    feat: add new-payment controller

--- payments/src/controllers/new-payment.ts
+++ payments/src/controllers/new-payment.ts
@@ -1,5 +1,32 @@
+import {
+    BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus,
+} from '@htickets/common';
 import { Request, Response } from 'express';
+import { Order } from '../models/order/order';
+import { stripe } from '../utils/stripe/stripe';
 
-export const newPayment = (req: Request, res: Response) => {
-    res.send({ sucess: true });
+export const newPayment = async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser.id) {
+        throw new NotAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+        throw new BadRequestError('Can not pay for a cancelled order');
+    }
+
+    await stripe.charges.create({
+        currency: 'usd',
+        amount: order.price * 100,
+        source: token,
+    });
+
+    res.status(201).send({ success: true });
 };
