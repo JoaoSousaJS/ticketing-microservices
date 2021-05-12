1a1b738c282113b0a93afa8a971c8a8b0cebb07b
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-12 12:27:02 +0930

    feat: add paymentCretedEvent to export

--- common/src/index.ts
+++ common/src/index.ts
@@ -22,3 +22,5 @@ export * from './events/order/order-cancelled-event'
 export * from './events/order/order-created-event'
 
 export * from './events/expiration/expiration-completed-event'
+
+export * from './events/payment/payment-created-event'
