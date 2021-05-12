eb95c5664f55152f0de5c10c6961d3d5ebdb4679
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-12 23:07:18 +0930

    feat: add PaymentCreatedListener class

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,28 @@
+import {
+    Listener, OrderStatus, PaymentCreatedEvent, Subjects,
+} from '@htickets/common';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/orders/orders';
+import { queueGroupName } from './types/queue-group-name';
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated
+
+    queueGroupName = queueGroupName
+
+    async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
+        const order = await Order.findById(data.orderId);
+
+        if (!order) {
+            throw new Error('Order not found');
+        }
+
+        order.set({
+            status: OrderStatus.Complete,
+        });
+
+        await order.save();
+
+        msg.ack();
+    }
+}
