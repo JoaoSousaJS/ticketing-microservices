cb368e0d052625d99b98d4dc1ad99f69e5b039db
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-18 00:15:47 +0930

    feat: add function to purchase a ticket

--- client/src/pages/tickets/[ticketId].tsx
+++ client/src/pages/tickets/[ticketId].tsx
@@ -1,5 +1,53 @@
-const TicketShow = () => {
-  return <div>Enter</div>
+import { Box, Container, Text } from '@chakra-ui/react'
+import { buildClient } from 'api/build-client'
+import Button from 'components/Button'
+import { useRequest } from 'hooks/use-request'
+import { GetServerSideProps } from 'next'
+
+type TicketProps = {
+  id: string
+  price: number
+  title: string
+  userId: string
+  version: number
+}
+
+const TicketShow = ({ price, title, id }: TicketProps) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post'
+  })
+  return (
+    <Container centerContent>
+      <Box>
+        <Text fontSize="7xl">{title}</Text>
+        <Text fontSize="2xl">Price: {price}</Text>
+        <Button
+          onClick={async () =>
+            await doRequest({
+              ticketId: id
+            })
+          }
+        >
+          Purchase
+        </Button>
+        {errors && errors}
+      </Box>
+    </Container>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const client = buildClient(ctx)
+  const { ticketId } = ctx.query
+
+  const { data } = await client.get(`/api/tickets/${ticketId}`)
+
+  return {
+    props: {
+      ...data
+    }
+  }
 }
 
 export default TicketShow
