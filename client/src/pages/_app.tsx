95899bd98e937f4e47170fb4d7b1fb84c545d278
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-19 12:44:07 +0930

    feat: add stripe provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 import { buildClient } from 'api/build-client'
 import Base from 'components/Base'
 import { AppContext, AppProps } from 'next/app'
@@ -8,23 +10,28 @@ import { GlobalStyles } from 'styles/global'
 import theme from 'styles/theme'
 
 const App = ({ Component, pageProps, currentUser }: AppProps) => {
+  const stripePromise = loadStripe(
+    'pk_test_51IorLAB0Pnttlxpw8XlnOpAlP3otqb47Hvtlm1OwjzpcYbfu4ub6KYK5jKkvr1Ib2Wr5Z4VJUBY8qayU1V0WeSf600OkTrPYPw'
+  )
   return (
     <>
       <ChakraProvider>
         <ThemeProvider theme={theme}>
-          <Head>
-            <title>Boiler Plate</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-            />
-          </Head>
-          <Base currentUser={currentUser} />
-          <GlobalStyles />
-          <Component currentUser={currentUser} {...pageProps} />
+          <Elements stripe={stripePromise}>
+            <Head>
+              <title>Boiler Plate</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+              />
+            </Head>
+            <Base currentUser={currentUser} />
+            <GlobalStyles />
+            <Component currentUser={currentUser} {...pageProps} />
+          </Elements>
         </ThemeProvider>
       </ChakraProvider>
     </>
