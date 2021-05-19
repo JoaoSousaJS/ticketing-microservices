3b0a0f87318b6f128c05a227c6224f902c786cc6
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-19 16:28:55 +0930

    feat: change message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -14,7 +14,7 @@ const start = async () => {
         useUnifiedTopology: true,
         useCreateIndex: true,
     });
-    console.log('connected to mongodb');
+    console.log('connected to mongodb and NATS');
     app.listen(3000, () => {
         console.log('listening on port 3000!');
     });
